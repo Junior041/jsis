bd6fd48aabf5b8366143df707746b90b6c3e5ca3
ismael <ismaeljunior1508@gmail.com>
2024-07-19 08:54:46 -0300

    feat: adicionado bearer auth no swagger

--- src/main/java/br/dev/ismael/jsis/domain/infra/swagger/SwaggerConfig.java
+++ src/main/java/br/dev/ismael/jsis/domain/infra/swagger/SwaggerConfig.java
@@ -1,9 +1,14 @@
 package br.dev.ismael.jsis.domain.infra.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -11,6 +16,23 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .info(new Info().title("JSIS").description("API TESTES JUNIOR").version("1"));
+                .components(
+                        new Components()
+                                .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")
+                                )
+                )
+                .security(List.of(new SecurityRequirement().addList("bearerAuth")))
+                .info(
+                        new Info()
+                                .title("JSIS")
+                                .description("API TESTES JUNIOR")
+                                .version("1")
+                );
     }
+
 }
