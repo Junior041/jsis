bf6dfbdc694cd63311d85b57ff4acca84ddab1d8
ismael <ismaeljunior1508@gmail.com>
2024-07-26 11:09:33 -0300

    feat: controller de criar endereço da pessoa

--- src/main/java/br/dev/ismael/jsis/domain/infra/http/controllers/endereco/CreateEnderecoPessoaController.java
+++ src/main/java/br/dev/ismael/jsis/domain/infra/http/controllers/endereco/CreateEnderecoPessoaController.java
@@ -3,6 +3,7 @@ package br.dev.ismael.jsis.domain.infra.http.controllers.endereco;
 import br.dev.ismael.jsis.domain.application.cases.enderecoPessoa.CreateEnderecoPessoaUseCase;
 import br.dev.ismael.jsis.domain.application.dto.enderecoPessoa.EnderecoPessoaRequestDTO;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
+@RequestMapping("/endereco/pessoa")
 @RestController
+@Tag(name = "Endereco")
 public class CreateEnderecoPessoaController {
 
     @Autowired
